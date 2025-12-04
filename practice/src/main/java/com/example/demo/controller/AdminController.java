16eac64489698e9a50370d046fe969d59f8199e2
ami-muto <a.muto.cingroup2012@gmail.com>
2025-12-05 00:16:15 +0900

    詳細取得、編集機能、ログアウトボタンの追加

--- practice/src/main/java/com/example/demo/controller/AdminController.java
+++ practice/src/main/java/com/example/demo/controller/AdminController.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.entity.Contact;
+import com.example.demo.form.EditContactForm;
 import com.example.demo.service.ContactService;
 
 @Controller
@@ -16,19 +20,65 @@ import com.example.demo.service.ContactService;
 public class AdminController {
 	@Autowired
 	private ContactService contactService;
+
 	@GetMapping("/contacts")
 	public String contactList(Model model) {
 		List<Contact> contactList = contactService.getAllContacts();
 		model.addAttribute("contacts", contactList);
 		return "contactList";
 	}
+
 	@GetMapping("/signup")
 	public String signup() {
 		return "signup";
 	}
+
 	@GetMapping("/signin")
 	public String signin() {
 		return "signin";
+	}
+
+	@GetMapping("/contacts/{id}")
+	public String detail(@PathVariable Long id, Model model) {
+		Contact contact = contactService.getContactById(id);
+		model.addAttribute("contact", contact);
+		return "detail";
+	}
+	
+	@GetMapping("/contacts/{id}/edit")
+	public String edit(@PathVariable Long id, Model model) {
+		Contact contact = contactService.getContactById(id);
+		
+		EditContactForm editcontactform = new EditContactForm();
+		editcontactform.setLastName(contact.getLastName());
+		editcontactform.setFirstName(contact.getFirstName());
+		editcontactform.setEmail(contact.getEmail());
+		editcontactform.setPhone(contact.getPhone());
+		editcontactform.setZipCode(contact.getZipCode());
+		editcontactform.setAddress(contact.getAddress());
+		editcontactform.setBuildingName(contact.getBuildingName());
+		editcontactform.setContactType(contact.getContactType());
+		editcontactform.setBody(contact.getBody());
+		
+		model.addAttribute("editContactForm", editcontactform);
+		model.addAttribute("contactId", contact.getId());
+		return "edit";
+	}
+	
+	@PostMapping("/contacts/{id}/edit")
+	public String update(@PathVariable Long id, @ModelAttribute EditContactForm editContactForm) {
+		contactService.updateContact(id, editContactForm);
+		return"redirect:/admin/contacts";
+	}
+	@PostMapping("/contacts/{id}")
+	public String delete(@PathVariable Long id) {
+		contactService.deleteContact(id);
+		return"redirect:/admin/contacts";
+	}
+	
+	@PostMapping("/logout")
+	public String logout() {
+	return"redirect:/signin";
 	}
 
 }
--- practice/src/main/java/com/example/demo/service/ContactService.java
+++ practice/src/main/java/com/example/demo/service/ContactService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.example.demo.entity.Contact;
 import com.example.demo.form.ContactForm;
+import com.example.demo.form.EditContactForm;
 
 public interface ContactService {
 	
@@ -12,5 +13,11 @@ public interface ContactService {
 	
 	//一覧取得メソッドを追加
 	List<Contact> getAllContacts();
-
+	Contact getContactById(Long id);
+	
+	//編集（更新）
+	void updateContact(Long id, EditContactForm editcontactForm);
+	
+	//削除
+	void deleteContact(Long id);
 }
