c59c8ec9c905b802c0e754a2b5cf2a4eba18f85f
ami-muto <a.muto.cingroup2012@gmail.com>
2025-12-21 23:36:04 +0900

    getter/setter の部分を @Data アノテーションに置き換え

--- practice/src/main/java/com/example/demo/entity/Admin.java
+++ practice/src/main/java/com/example/demo/entity/Admin.java
@@ -1,42 +1,34 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+import lombok.Data;
+
+@Data
 @Entity
+@Table(name = "admin")
 public class Admin {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
 	
+	@Column(name = "last_name")
 	private String lastName;
+	
+	@Column(name = "first_name")
 	private String firstName;
+	
+	@Column(name = "email")
 	private String email;
+	
+	@Column(name = "password")
 	private String password;
 	
-	public Long getId() { return id; }
-	public void setId(Long id) {this.id = id; }
 	
-	public String getLastName() { return lastName; }
-	public void setLastName(String lastName) { this.lastName = lastName; }
-	
-	public String getFirstName() { return firstName; }
-	public void setFirstName(String firstName) { this.firstName = firstName; }
-	
-	public String getEmail() { return email; }
-	public void setEmail(String email) { this.email = email; }
-	
-	public String getPassword() { return password; }
-	public void setPassword(String password) { this.password = password; }
-	
-	
-	
-	
-	
-	
-	
-	
-
 }
--- practice/src/main/java/com/example/demo/form/AdminForm.java
+++ practice/src/main/java/com/example/demo/form/AdminForm.java
@@ -4,6 +4,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import lombok.Data;
+
+@Data
 public class AdminForm {
 	@NotBlank(message = "姓は必須です")
 	private String lastName;
@@ -18,45 +21,4 @@ public class AdminForm {
 	@NotBlank(message = "パスワードは必須です")
 	@Size(min = 6, message = "パスワードは６文字以上で入力してください")
 	private String password;
-
-	public String getLastName() {
-		return lastName;
-
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-
-	}
-
-	public String getFirstName() {
-		return firstName;
-
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-
-	}
-
-	public String getEmail() {
-		return email;
-
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-
-	}
-
-	public String getPassword() {
-		return password;
-
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-
-	}
-
 }
